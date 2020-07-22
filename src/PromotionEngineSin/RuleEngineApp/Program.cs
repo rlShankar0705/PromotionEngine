5a98e3cd9df0af70a843800f07547d9b76b77484
Gunti Rao <grao@hudson-advisors.com>
2020-07-22 21:37:55 +0530

    Test console app for promotion engine test.

--- src/PromotionEngineSin/RuleEngineApp/Program.cs
+++ src/PromotionEngineSin/RuleEngineApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RuleEnginePattern;
+using System;
 
 namespace RuleEngineApp
 {
@@ -6,7 +7,14 @@ namespace RuleEngineApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var active = new DiscountCalculator();
+            var s1 = active.CalculateDiscountPercentage("A,B,C"); // 100 - Pass
+            Console.WriteLine("Total = {0}", s1);
+            var s2 = active.CalculateDiscountPercentage("A,A,A,A,A,B,B,B,B,B,C"); // 370 - Pass
+            Console.WriteLine("Total = {0}", s2);
+            var s3 = active.CalculateDiscountPercentage("A,A,A,,B,B,B,B,B,C,D"); // 280 - Pass
+            Console.WriteLine("Total = {0}", s3);
+            Console.ReadKey();
         }
     }
 }
