bef445ea0ac64e6fc78c2dcb81643d44dd7a6599
Gunti Rao <grao@hudson-advisors.com>
2020-07-22 16:07:34 +0530

    Adding SKUIds and Active Promotion details

---
+++ src/PromotionEngineSin/RuleEngineDatabase/ActivePromotion.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace RuleEngineDatabase
+{
+    public class ActivePromotion
+    {
+        public string SKUId { get; set; }
+        public int DiscountOnUnitCount { get; set; }
+
+        public bool IsCombinationDiscount { get; set; }
+        public string CombinationSKUID { get; set; }
+
+        public int IsAppliedPromotionCountForSKUId { get; set; }
+
+        public IEnumerable<ActivePromotion> GetActivePromotion()
+        {
+            List<ActivePromotion> items = new List<ActivePromotion>();
+            items.Add(new ActivePromotion()
+            {
+                SKUId = "A",
+                DiscountOnUnitCount = 3,
+                IsCombinationDiscount = false,
+                CombinationSKUID = null,
+                IsAppliedPromotionCountForSKUId = 1
+            });
+            items.Add(new ActivePromotion()
+            {
+                SKUId = "B",
+                DiscountOnUnitCount = 2,
+                IsCombinationDiscount = false,
+                CombinationSKUID = null,
+                IsAppliedPromotionCountForSKUId = 1
+            });
+            items.Add(new ActivePromotion()
+            {
+                SKUId = "C",
+                DiscountOnUnitCount = 0,
+                IsCombinationDiscount = true,
+                CombinationSKUID = "C,D",
+                IsAppliedPromotionCountForSKUId = 1
+            });
+            items.Add(new ActivePromotion()
+            {
+                SKUId = "A",
+                DiscountOnUnitCount = 0,
+                IsCombinationDiscount = true,
+                CombinationSKUID = "D,C",
+                IsAppliedPromotionCountForSKUId = 1
+            });
+
+            return items;
+        }
+    }
+}
---
+++ src/PromotionEngineSin/RuleEngineDatabase/SKU.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace RuleEngineDatabase
+{
+    public class SKU
+    {
+        public string SKUId { get; set; }
+        public long AmountPerUnit { get; set; }
+
+        public IEnumerable<SKU> GETUnitPriceForSKUID()
+        {
+            List<SKU> items = new List<SKU>();
+            items.Add(new SKU() { SKUId = "A", AmountPerUnit = 50 });
+            items.Add(new SKU() { SKUId = "B", AmountPerUnit = 30 });
+            items.Add(new SKU() { SKUId = "C", AmountPerUnit = 20 });
+            items.Add(new SKU() { SKUId = "D", AmountPerUnit = 10 });
+
+            return items;
+        }
+    }
+}
